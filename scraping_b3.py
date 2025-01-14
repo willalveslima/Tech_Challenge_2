7b25e538dbfef291454796b0b0a494c8cb5f4289
Willian Alves Lima <w.alves.lima@gmail.com>
2025-01-14 18:02:05 -0300

    Adicionar lógica para download de arquivo CSV e conversão para Parquet

--- scraping_b3.py
+++ scraping_b3.py
@@ -1,4 +1,7 @@
+import os
 import time
+import pandas as pd
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -7,30 +10,55 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 # Configurações do ChromeDriver
 chrome_options = Options()
+
+# Diretório base de download
+download_directory = "C:\\Users\\walve\\Documents\\FIAP\\Fase 2 BigData\\Tech Challenge\\Tech_Challenge_2\\pregoes"
+
+# Configurações do ChromeDriver para o diretório de download
 chrome_options.add_experimental_option('prefs', {
-    "download.default_directory": "C:\\Users\\walve\\Documents\\FIAP\\Fase 2 BigData\\Tech Challenge\\Tech_Challenge_2\\pregoes",  # Altere para o diretório desejado
+    "download.default_directory": download_directory,
     "download.prompt_for_download": False,
     "download.directory_upgrade": True,
     "safebrowsing.enabled": True
 })
 
-# Inicializa o ChromeDriver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+# Nome do arquivo no formato IBOVDia_dd-mm-aa.csv
+today_date = datetime.today().strftime('%d-%m-%y')
+file_name = f'IBOVDia_{today_date}.csv'
+downloaded_file_path = os.path.join(download_directory, file_name)
 
-# Acessa a URL desejada
-url = "https://sistemaswebb3-listados.b3.com.br/indexPage/day/IBOV?language=pt-br"
-driver.get(url)
+# Verifica se o arquivo já existe
+if not os.path.exists(downloaded_file_path):
+    # Inicializa o ChromeDriver
+    driver = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()), options=chrome_options)
 
-# Aguarda o carregamento da página
-time.sleep(5)
+    # Acessa a URL desejada
+    url = "https://sistemaswebb3-listados.b3.com.br/indexPage/day/IBOV?language=pt-br"
+    driver.get(url)
 
+    # Aguarda o carregamento da página
+    time.sleep(5)
 
-# Encontra e clica no botão de download (ajuste o seletor conforme necessário)
-download_button = driver.find_element(By.LINK_TEXT, 'Download')  # Ajuste o texto do link conforme necessário
-download_button.click()
+    # Encontra e clica no botão de download (ajuste o seletor conforme necessário)
+    # Ajuste o texto do link conforme necessário
+    download_button = driver.find_element(By.LINK_TEXT, 'Download')
+    download_button.click()
 
-# Aguarda o download ser concluído
-time.sleep(10)
+    # Aguarda o download ser concluído
+    time.sleep(10)
 
-# Fecha o navegador
-driver.quit()
+    # Fecha o navegador
+    driver.quit()
+
+    # Lê o arquivo CSV ignorando a primeira linha e as duas últimas, usando a codificação 'latin1'
+    df = pd.read_csv(downloaded_file_path, skiprows=1, encoding='latin1')
+    df = df[:-2]
+
+    # Salva o DataFrame como um arquivo Parquet
+    parquet_file_path = os.path.join(
+        download_directory, f'IBOVDia_{today_date}.parquet')
+    df.to_parquet(parquet_file_path)
+    
+else:
+    print(f"O arquivo {file_name} já existe. O download foi cancelado.")
